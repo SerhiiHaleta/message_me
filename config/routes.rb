5194194f9a78f45caea71a1ce9a1420f089e2934
Serhii Haleta <galeta.sergey@gmail.com>
2022-10-25 23:42:11 +0200

    Favicon added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
