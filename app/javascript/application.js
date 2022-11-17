d1246ab294b7cac13373fb2a0113e8e61e727753
Serhii Haleta <galeta.sergey@gmail.com>
2022-11-17 23:45:15 +0100

    MessageMe application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,33 @@ import "controllers"
 import * as jquery from "jquery"
 import "semantic-ui" 
 
-$(document).on("turbo:load", () => $(".ui.dropdown").dropdown());
+const scroll_bottom = () => {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight)
+
+  }
+}
+
+const submit_message = () => {
+  $("#message_body").on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+
+      e.target.value = "";
+    }
+  });
+}
+
+
+$(document).on("turbo:load", () => {
+  $(".ui.dropdown").dropdown();
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade');
+  });
+  submit_message();
+  scroll_bottom();
+});
+import "channels"
